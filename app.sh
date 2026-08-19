4ab38ec61a5074e376c6923bdcde258a07daa52f
gjdileep-byte <gjdileep@gmail.com>
2026-08-19 16:48:25 +0530

    Add Jenkins automation demo script

---
+++ app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "======================================"
+echo " Jenkins Automation Demo Application"
+echo "======================================"
+echo "Application Version:"
+cat version.txt
+echo "Hostname:"
+hostname
+echo "Execution User:"
+whoami
+echo "Application execution completed."
